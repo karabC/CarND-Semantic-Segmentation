31a11aaeeadf197c03755dd1f16b5a28c2cba2ab
Karab <karab.sze@oocll.com>
2018-12-27 21:08:07 +0800

    Fix typo

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
         batch_count=0
         for image, label in get_batches_fn(batch_size):
             batch_count+=1
-            _, loss=sess.run([train_op,cross_entropy_loss], feed_dict={input_image:image, correct_label:label, keep_prob:keep_prob_, learning_rate:learning_rate_})
+            _, loss=sess.run([train_op,cross_entropy_loss], feed_dict={input_image:image, correct_label:label, keep_prob:keep_prob, learning_rate:learning_rate})
             print("Epoch no: "+str(epoch+1)+", Batch number: "+str(batch_count)+", Loss:  "+str(loss))
     #return loss
 
